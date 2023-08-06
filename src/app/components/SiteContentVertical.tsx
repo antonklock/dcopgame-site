4961541353f5fcf0e34072aecb5c9cff29c02768
Anton Klock <klock.anton@gmail.com>
2023-08-06 20:20:27 +0200

    fixing vertical

---
+++ src/app/components/SiteContentVertical.tsx
@@ -0,0 +1,77 @@
+"use client";
+
+import { checkCustomRoutes } from "next/dist/lib/load-custom-routes";
+import DcopBackground from "./DcopBackground";
+import GraphicElements from "./GraphicElements";
+import Image from "next/image";
+import SteamButton from "./SteamButton";
+
+export default function SiteContent() {
+  return (
+    <>
+      <div
+        className="h-full w-full flex items-center justify-center"
+        style={bgStyle}
+      >
+        <div className="h-full flex flex-col justify-between px-20 max-w-3xl py-10">
+          <div className="flex flex-col items-center pt-20">
+            <div className="flex justify-center">
+              <Image
+                src="/SVG/dcop_logo_01.svg"
+                alt="dcop logo"
+                width={1000}
+                height={1080}
+                // layout="fill"
+              />
+            </div>
+
+            <div className="py-10">
+              <Image
+                src="/credits.png"
+                alt="dcop credits"
+                width={1300}
+                height={205}
+              />
+            </div>
+
+            <div className="pt-4">
+              <SteamButton />
+            </div>
+          </div>
+
+          <div className="flex flex-row justify-center mt-10">
+            <Image
+              src="/SVG/unity_logo.svg"
+              alt="unity logo"
+              width={120 / 1.5}
+              height={100 / 1.5}
+              className="opacity-50"
+            />
+            <Image
+              src="/SVG/steam_logo.svg"
+              alt="unity logo"
+              width={150 / 1.5}
+              height={100 / 1.5}
+              className="opacity-50 ml-7"
+            />
+            <Image
+              src="/SVG/arduino_logo.svg"
+              alt="unity logo"
+              width={65 / 1.5}
+              height={50 / 1.5}
+              className="opacity-50 ml-7"
+            />
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+const bgStyle = {
+  //add background image
+  backgroundImage: "url(/dcop_bg.jpg)",
+  backgroundSize: "cover",
+  backgroundPosition: "center",
+  backgroundRepeat: "no-repeat",
+};
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,55 @@
-import DcopBackground from "./components/DcopBackground";
-import GraphicElements from "./components/GraphicElements";
+"use client";
+
 import SiteContent from "./components/SiteContent";
+import { useState, useLayoutEffect, useEffect } from "react";
+import SiteContentVertical from "./components/SiteContentVertical";
 
 export default function Home() {
-  return (
-    <div className="h-screen w-screen flex items-center justify-center bg-black">
-      <SiteContent />
-    </div>
-  );
+  const [width, height] = useWindowSize();
+  const [displayVertical, setDisplayVertical] = useState(false);
+
+  useEffect(() => {
+    if (isVertical(width, height)) {
+      setDisplayVertical(true);
+      console.log("Vertical");
+    } else {
+      setDisplayVertical(false);
+      console.log("Horizontal");
+    }
+  }, [width, height]);
+
+  if (displayVertical) {
+    return (
+      <div className="h-screen w-screen flex items-center justify-center bg-black">
+        <SiteContentVertical />
+      </div>
+    );
+  } else {
+    return (
+      <div className="h-screen w-screen flex items-center justify-center bg-black">
+        <SiteContent />
+      </div>
+    );
+  }
+}
+
+function isVertical(width: number, height: number) {
+  if (width < 1050) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function useWindowSize() {
+  const [size, setSize] = useState([0, 0]);
+  useLayoutEffect(() => {
+    function updateSize() {
+      setSize([window.innerWidth, window.innerHeight]);
+    }
+    window.addEventListener("resize", updateSize);
+    updateSize();
+    return () => window.removeEventListener("resize", updateSize);
+  }, []);
+  return size;
 }
