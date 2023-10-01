3508348780b9921363eec76addd35b333f630346
Anton Klock <anton@klockworks.se>
2023-10-02 00:56:27 +0200

    button text should not be on two lines now

--- src/app/components/SteamButton.tsx
+++ src/app/components/SteamButton.tsx
@@ -11,7 +11,7 @@ export default function SteamButton() {
   return (
     <button
       onClick={handleOpenSteam}
-      className="flex items-center justify-center w-56 h-16 px-8 text-lg font-semibold text-white rounded-lg transition ease-in-out duration-500 bg-gradient-to-b from-dcopBrightBlue to-dcopDarkBlue hover:scale-105 hover:bg-gradient-to-b hover:from-dcopBrightOrange hover:to-dcopDarkOrange"
+      className="flex items-center justify-center w-56 h-16 text-lg font-semibold text-white rounded-lg transition ease-in-out duration-500 bg-gradient-to-b from-dcopBrightBlue to-dcopDarkBlue hover:scale-105 hover:bg-gradient-to-b hover:from-dcopBrightOrange hover:to-dcopDarkOrange"
     >
       <span className="mr-4">Get the game</span>
       <Image
